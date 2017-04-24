c4044a922f4a04433fd31314d5c660be1ae3ba75
Joe Morel <joe@Joes-MacBook-Pro.local>
2017-04-24 13:35:54 -0400

    more db changes

--- web/index.php
+++ web/index.php
@@ -2,6 +2,20 @@
 
 require('../vendor/autoload.php');
 
+$dbopts = parse_url(getenv('DATABASE_URL'));
+$app->register(new Csanquer\Silex\PdoServiceProvider\Provider\PDOServiceProvider('pdo'),
+               array(
+                'pdo.server' => array(
+                   'driver'   => 'pgsql',
+                   'user' => $dbopts["user"],
+                   'password' => $dbopts["pass"],
+                   'host' => $dbopts["host"],
+                   'port' => $dbopts["port"],
+                   'dbname' => ltrim($dbopts["path"],'/')
+                   )
+               )
+);
+
 $app = new Silex\Application();
 $app['debug'] = true;
 
@@ -26,6 +40,21 @@ $app->get('/', function() use($app) {
 $app->get('/cowsay', function() use ($app) {
 	$app['monolog']->addDebug('cowsay');
 	return "<pre>".\Cowsayphp\Cow::say("Cool beans")."</pre>";
+});
+
+$app->get('/db/', function() use($app) {
+  $st = $app['pdo']->prepare('SELECT name FROM test_table');
+  $st->execute();
+
+  $names = array();
+  while ($row = $st->fetch(PDO::FETCH_ASSOC)) {
+    $app['monolog']->addDebug('Row ' . $row['name']);
+    $names[] = $row;
+  }
+
+  return $app['twig']->render('database.twig', array(
+    'names' => $names
+  ));
 });
 
 $app->run();
