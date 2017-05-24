ea3e2ba752744b53be820570a618a2c6635af2a7
Joe Morel <joe@33mileradius.com>
2017-05-24 13:06:50 -0400

    fixing event loop stuff

--- index.js
+++ index.js
@@ -2,12 +2,14 @@
 
 var http = require('http');
 var url = require('url');
+var async = require('async');
 
 var pg = require('pg');
 
 var port = process.env.PORT || 8888;
 
 pg.defaults.ssl = true;
+
 
 function onRequest(request,response) {
     console.log("Request received.");
@@ -16,9 +18,11 @@ function onRequest(request,response) {
         client.query('SELECT * from salesforce.account;')
         .on('row', function(row) {
             response.write(JSON.stringify(row));
+        })
+        .on('end', function(result) {
+            response.end();
         });
     });
-    response.end();
 }
 
 http.createServer(onRequest).listen(port);
