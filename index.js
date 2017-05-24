17cfa19acc603186feb49ea9749a0e3c1b8fef2a
Joe Morel <joe@33mileradius.com>
2017-05-24 11:34:59 -0400

    Added postgre query

--- index.js
+++ index.js
@@ -3,13 +3,23 @@
 var http = require('http');
 var url = require('url');
 
+var pg = require('pg');
+
 var port = process.env.PORT || 8888;
+
+pg.defaults.ssl = true;
 
 function onRequest(request,response) {
     console.log("Request received.");
-    response.writeHead(200,{"Content-Type":"text/html"});
-    response.write("<html><body><h2>Hello, world!</h2></body></html>");
-    response.end();
+    response.writeHead(200,{"Content-Type":"text/json"});
+    pg.connect(process.env.DATABASE_URL, function(err, client) {
+        client.query('SELECT * from account;')
+        .on('row', function(row) {
+            response.write(JSON.stringify(row));
+            response.end();
+        });
+    });
+
 }
 
 http.createServer(onRequest).listen(port);
