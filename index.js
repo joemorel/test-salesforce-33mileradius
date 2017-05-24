64fdaf23f91372cbcbda24267880aa738913d78f
Joe Morel <joe@33mileradius.com>
2017-05-24 10:55:34 -0400

    New Node.js app

---
+++ index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var http = require('http');
+var url = require('url');
+
+function onRequest(request,response) {
+    console.log("Request received.");
+    response.writeHead(200,{"Content-Type":"text/html"});
+    response.write("<html><body><h2>Hello, world!</h2></body></html>");
+    response.end();
+}
+
+http.createServer(onRequest).listen(80);
+
+console.log("Server has started.");
