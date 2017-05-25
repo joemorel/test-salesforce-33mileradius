911521151b5123988061de3e98df7232730795f2
Joe Morel <joe@33mileradius.com>
2017-05-25 13:19:18 -0400

    Updating Express

--- app.js
+++ app.js
@@ -5,11 +5,27 @@
 
 const express = require('express');
 const app = express();
+const pg = require('pg');
+const connectionString = process.env.DATABASE_URL;
+const client = new pg.Client(connectionString);
 
 var port = process.env.PORT || 8888;
 
 app.get('/',function(req,res) {
     res.send('Hello, world!');
+});
+
+app.get('/account',function(req,res) {
+    client.connect();
+    const query = client.query(
+        "SELECT createddate, phone, website, name, description, accountnumber from salesforce.account");
+    query.on('row', function(row,err) {
+        res.write(JSON.stringify(row));
+    });
+    query.on('end', function() {
+        res.end();
+        client.end();
+    });
 });
 
 app.listen(port, function() {
