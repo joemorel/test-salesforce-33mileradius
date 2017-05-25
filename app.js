b044d24e953058a8d1b2577d7d2cd4c79ca16ee9
Joe Morel <joe@33mileradius.com>
2017-05-25 14:40:01 -0400

    Understanding body-parser

--- app.js
+++ app.js
@@ -6,23 +6,26 @@
 const express = require('express');
 const app = express();
 const pg = require('pg');
-const bodyParser = require('body-parser');
-app.use(bodyParser.json);
-app.use(bodyParser.urlencoded({extended: true}));
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 const connectionString = process.env.DATABASE_URL || "postgres://localhost:5432/testsalesforce";
 console.log(connectionString);
 const client = new pg.Client(connectionString);
-
+console.log("created PostgreSQL client");
 var port = process.env.PORT || 8888;
 
 app.get('/',function(req,res) {
+    console.log("hit root with GET");
     res.send('Hello, world!');
 });
 
 app.get('/account',function(req,res) {
+    var responseBody = req.body;
     client.connect();
     const query = client.query(
-        "SELECT createddate, phone, website, name, description, accountnumber from salesforce.account;");
+        "SELECT createddate, phone, website, name, description, accountnumber from salesforce.account");
     query.on('row', function(row,err) {
         res.write(JSON.stringify(row));
     });
@@ -34,18 +37,18 @@ app.get('/account',function(req,res) {
 
 app.post('/account',function(req,res) {
    client.connect();
-    console.log("Output:",req.body.phone);
+   console.log("Output:", req.body.phone);
    const query = client.query(
-       "INSERT INTO salesforce.account (phone, website, name, description, accountnumber) VALUES (",req.body.phone,",",req.body.website,",",req.body.name,",",req.body.description,req.body.accountnumber,");"
+       "INSERT INTO salesforce.account (phone, website, name, description, accountnumber) VALUES (",req.body.phone,",",req.body.website,",",req.body.name,",",req.body.description,req.body.accountnumber,")"
    );
    console.log(query.text());
-   query.on('end', function() {
-       res.write("OK");
+   query.on('end', function(result) {
+       res.write(result);
        res.end();
        client.end();
    });
 
-   query.on('err', function(err) {
+   query.on('error', function(err) {
        console.log(err);
    });
 });
